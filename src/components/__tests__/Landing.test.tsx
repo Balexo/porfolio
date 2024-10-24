968bb84250b678241f4d5a0e5e262ec36fa970ad
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-10-24 12:55:04 +0200

    Landing tested

--- src/components/__tests__/Landing.test.tsx
+++ src/components/__tests__/Landing.test.tsx
@@ -2,24 +2,44 @@ import Landing from "../Landing";
 import { ThemeProvider } from "styled-components";
 import theme from "../../theme";
 import { landingText } from "../../data/HomeData";
-import gsap from "gsap";
 import { render, screen } from "@testing-library/react";
 import { vi } from "vitest";
+import gsap from "gsap";
 
-describe(Landing, () => {
-  vi.mock("gsap", () => ({
-    to: vi.fn(),
-  }));
+vi.spyOn(gsap, "to").mockImplementation(
+  () =>
+    ({
+      stagger: 0.03,
+      kill: vi.fn(() => ({})),
+    }) as any,
+);
 
-  describe("Landing component", () => {
-    const renderWithTheme = (component: React.ReactElement) => {
-      return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
-    };
+describe("First letter of Landing Component", () => {
+  const renderWithTheme = (component: React.ReactElement) => {
+    return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
+  };
+  screen.debug();
+  it("renders landing text correctly", () => {
+    renderWithTheme(<Landing />);
 
-    it("renders landingText", () => {
-      renderWithTheme(<Landing />);
-      const paragraph = screen.getByText(landingText);
-      expect(paragraph).toBeInTheDocument();
+    const paragraph = screen.getByText(landingText[0]);
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it("applies animation to each character", () => {
+    renderWithTheme(<Landing />);
+
+    const chars = landingText.split("");
+    chars.forEach((_, i) => {
+      const span = document.querySelector(`.char-${i}`);
+      expect(span).toBeInTheDocument();
+      expect(span).toHaveClass(`char-${i}`);
     });
+
+    expect(gsap.to).toHaveBeenCalled();
+    expect(gsap.to).toHaveBeenCalledWith(
+      chars.map((_, i) => `.char-${i}`),
+      expect.any(Object),
+    );
   });
 });
